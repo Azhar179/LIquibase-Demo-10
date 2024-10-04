46eeb2aaa73a5175b42f454032247dc0aaee253a
Bhagwan <bhagwana@win.dowjones.net>
2024-10-04 12:48:10 +0530

    Code Commit

---
+++ LIquibase-Demo/src/main/java/LIquibase_Demo/LIquibase_Demo/LIquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package LIquibase_Demo.LIquibase_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LIquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LIquibaseDemoApplication.class, args);
+	}
+
+}
---
+++ LIquibase-Demo/src/test/java/LIquibase_Demo/LIquibase_Demo/LIquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package LIquibase_Demo.LIquibase_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LIquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
